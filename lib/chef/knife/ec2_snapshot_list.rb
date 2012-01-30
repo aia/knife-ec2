00430dec35b40d6e4c5761ae68ea356ef5ec42b6
Artem <artem@veremey.net>
2012-01-29 20:29:30 -0800

    WIP: Snapshot List

---
+++ lib/chef/knife/ec2_snapshot_list.rb
@@ -0,0 +1,76 @@
+#
+# Author:: Artem Veremey (<artem@veremey.net>)
+# Copyright:: Copyright (c) Artem Veremey
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/knife/ec2_base'
+
+class Chef
+  class Knife
+    class Ec2SnapshotList < Knife
+
+      include Knife::Ec2Base
+
+      banner "knife ec2 snapshot list (options)"
+      
+      option :allregions,
+        :short => "--all",
+        :long => "--all-regions",
+        :boolean => true,
+        :description => "List servers in all regions",
+        :proc => Proc.new { |f| Chef::Config[:knife][:allregions] = f }
+
+      def run
+        $stdout.sync = true
+
+        validate!
+        
+        regions = []
+        if Chef::Config[:knife][:allregions]
+          connection.describe_regions.body["regionInfo"].each do |region|
+            regions << region["regionName"]
+          end
+        else
+          regions << locate_config_value(:region)
+        end
+
+        regions.each do |region|
+          snapshot_list = list_snapshots(region)
+
+          puts "Listing snapshots in region #{region}"
+          puts ui.list(snapshot_list, :columns_across, 7)
+        end
+
+      end
+      
+      def list_snapshots(region = nil)
+        snapshot_list = ["ID", "Volume ID", "Name", "Size", "Description", "Created", "State"]
+        snapshot_list.map!{ |f| ui.color(f, :bold) }
+        connection(region).snapshots.all.each do |snapshot|
+          snapshot_list << snapshot.id.to_s
+          snapshot_list << snapshot.volume_id.to_s
+          snapshot_list << snapshot.tags["Name"].to_s
+          snapshot_list << [snapshot.volume_size.to_s, "G"].join
+          snapshot_list << snapshot.description.to_s
+          snapshot_list << snapshot.created_at.to_s
+          snapshot_list << "#{snapshot.state.to_s}(#{snapshot.progress.to_s})"
+        end
+        
+        return snapshot_list
+      end
+    end
+  end
+end
