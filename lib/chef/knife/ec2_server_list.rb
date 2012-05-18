a18df484dadcec6a66a0969521675a7559fa0cf2
Artem Veremey <artem@veremey.net>
2012-05-17 23:07:51 -0700

    Updates for fixes in fog 1.3.1

--- lib/chef/knife/ec2_server_list.rb
+++ lib/chef/knife/ec2_server_list.rb
@@ -85,7 +85,7 @@ class Chef
           server_list << server.flavor_id.to_s
           server_list << server.image_id.to_s
           server_list << server.key_name.to_s
-          server_list << "[#{server.groups.values_at(* server.groups.each_index.select {|i| i.odd?}).join(', ')}]"
+          server_list << "[#{server.groups.join(", ")}]"
           server_list << color_state(:server, server.state.to_s.downcase)
         end
         
